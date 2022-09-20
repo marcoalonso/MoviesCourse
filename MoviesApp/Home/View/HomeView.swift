5fda200608ddac64a93f6aba4cf08b5423ec96a1
Marco Alonso <mrodriguez@alquimiapay.com>
2022-09-19 23:01:26 -0500

    4 Adding tableview to HomeView

--- MoviesApp/Home/View/HomeView.swift
+++ MoviesApp/Home/View/HomeView.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class HomeView: UIViewController {
     
+    
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
+    
     //Es una propiedad de nuestro viewModel
     private var router = HomeRouter()
     private var viewModel = HomeViewModel()
@@ -20,4 +24,17 @@ class HomeView: UIViewController {
     }
 
 
+}
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
